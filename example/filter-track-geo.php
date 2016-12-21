10bc782e4a9d72c85acca5ba0057a0c10f0bcbb6
George Janak <gjanak@caci.co.uk>
2016-12-21 15:37:44 +0000

    - psr1/psr2 refactoring - phpdoc fixes - fixed error log and small typos - bumped up the version to php5.4

--- example/filter-track-geo.php
+++ example/filter-track-geo.php
@@ -1,6 +1,6 @@
 <?php
-require_once('../lib/Phirehose.php');
-require_once('../lib/OauthPhirehose.php');
+require_once '../lib/Phirehose.php';
+require_once '../lib/OauthPhirehose.php';
 require_once 'twitter-auth-config.php';
 
 /**
@@ -8,29 +8,31 @@ require_once 'twitter-auth-config.php';
  */
 class FilterTrackConsumer extends OauthPhirehose
 {
-  /**
-   * Enqueue each status
-   *
-   * @param string $status
-   */
-  public function enqueueStatus($status)
-  {
-    /*
-     * In this simple example, we will just display to STDOUT rather than enqueue.
-     * NOTE: You should NOT be processing tweets at this point in a real application, instead they should be being
-     *       enqueued and processed asyncronously from the collection process.
+    /**
+     * Enqueue each status
+     *
+     * @param string $status
      */
-    $data = json_decode($status, true);
-    if (is_array($data) && isset($data['user']['screen_name'])) {
-      print $data['user']['screen_name'] . ': ' . urldecode($data['text']) . "\n";
+    public function enqueueStatus($status)
+    {
+        /*
+       * In this simple example, we will just display to STDOUT rather than enqueue.
+       * NOTE: You should NOT be processing tweets at this point in a real application, instead they should be being
+       *       enqueued and processed asyncronously from the collection process.
+       */
+        $data = json_decode($status, true);
+        if (is_array($data) && isset($data['user']['screen_name'])) {
+            print $data['user']['screen_name'] . ': ' . urldecode($data['text']) . "\n";
+        }
     }
-  }
 }
 
 // Start streaming
 $sc = new FilterTrackConsumer(OAUTH_TOKEN, OAUTH_SECRET, Phirehose::METHOD_FILTER);
-$sc->setLocations(array(
-       array(-122.75, 36.8, -121.75, 37.8), // San Francisco
-       array(-74, 40, -73, 41),             // New York
-   ));
+$sc->setLocations(
+    [
+        [-122.75, 36.8, -121.75, 37.8], // San Francisco
+        [-74, 40, -73, 41],             // New York
+    ]
+);
 $sc->consume();
